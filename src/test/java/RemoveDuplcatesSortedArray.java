3d19afbe400af203f070c9cd80142dbfb7ea3e60
halleynora <halley.short@gmail.com>
2022-08-01 15:20:59 -0700

    pushing up more practice algorithms

--- src/test/java/RemoveDuplcatesSortedArray.java
+++ src/test/java/RemoveDuplcatesSortedArray.java
@@ -1,24 +1,38 @@
 public class RemoveDuplcatesSortedArray {
 
+//    public static int removeDuplicates(int[] nums) {
+//        if (nums == null) {
+//            throw new IllegalArgumentException("Input is invalid");
+//        }
+//
+//        int len = nums.length;
+//        if (len <= 1) {
+//            return len;
+//        }
+//
+//        int insertPos = 0;
+//        for (int i = 1; i < len; i++) {
+//            if (nums[i] != nums[insertPos]) {
+//                nums[++insertPos] = nums[i];
+//            }
+//        }
+//
+//        return insertPos + 1;
+//
+//    }
+
     public static int removeDuplicates(int[] nums) {
-        if (nums == null) {
-            throw new IllegalArgumentException("Input is invalid");
-        }
 
-        int len = nums.length;
-        if (len <= 1) {
-            return len;
-        }
+        int lPointer = 1;
 
-        int insertPos = 0;
-        for (int i = 1; i < len; i++) {
-            if (nums[i] != nums[insertPos]) {
-                nums[++insertPos] = nums[i];
+        for (int rPointer = 1; rPointer <= nums.length; rPointer++) {
+            if (nums[rPointer] != nums[rPointer -1]) {
+                nums[lPointer] = nums[rPointer];
+                rPointer += 1;
             }
         }
 
-        return insertPos + 1;
-
+        return lPointer;
     }
 
     public static void main(String[] args)
